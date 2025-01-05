c20d468c2e8ded8b6ed044c5da93b528e8e87eda
Gideon Charles Maoga <138870694+GideonDevRel@users.noreply.github.com>
2025-01-05 14:32:29 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Portfolio',
+  title: 'Gideon DevRel',
   description: 'My personal portfolio showcasing my work and experience',
 }
 
